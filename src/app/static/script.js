f529bfd9354adb40414418cd8b70ad17d344ce77
julianortlieb <mygames@julian-ortlieb.de>
2021-05-14 09:54:59 +0200

    Add drawArea

---
+++ src/app/static/script.js
@@ -0,0 +1,55 @@
+var mousePressed = false;
+var lastX, lastY;
+var ctx;
+
+function initCanvas() {
+    ctx = document.getElementById("drawArea").getContext("2d");
+
+    document.getElementById("drawArea").addEventListener('mousedown', function (e) {
+        mousePressed = true;
+        Draw(e.pageX - this.getBoundingClientRect().x, e.pageY - this.getBoundingClientRect().y, false);
+    });
+
+    document.getElementById("drawArea").addEventListener('mousemove', function (e) {
+        if(mousePressed)
+            Draw(e.pageX - this.getBoundingClientRect().x, e.pageY - this.getBoundingClientRect().y, true);
+    });
+
+    document.getElementById("drawArea").addEventListener('mouseup', function (e) {
+        mousePressed = false;
+    });
+
+    document.getElementById("drawArea").addEventListener('mouseleave', function (e) {
+        mousePressed = false;
+    });
+}
+
+function Draw(x, y, isDown) {
+    if (isDown) {
+        ctx.beginPath();
+        ctx.strokeStyle = "black";
+        ctx.lineWidth = 12;
+        ctx.lineJoin = "round";
+        ctx.moveTo(lastX, lastY);
+        ctx.lineTo(x, y);
+        ctx.closePath();
+        ctx.stroke();
+    }
+    lastX = x; lastY = y;
+}
+
+function clearArea() {
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
+
+function convertToImage() {
+    let canvas = document.getElementById("drawArea");
+    let image = new Image();
+    image.src = canvas.toDataURL();
+    return image;
+  }
+
+document.addEventListener('DOMContentLoaded', function () {
+    initCanvas();
+  }, false);
